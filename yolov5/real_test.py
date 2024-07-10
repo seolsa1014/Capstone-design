796ecbaf80beb6b83b43491081082c3ac09003a1
seol <seolsa1014@gmail.com>
2024-07-10 17:19:19 +0900

    mid report complete

--- yolov5/real_test.py
+++ yolov5/real_test.py
@@ -1,4 +1,4 @@
 import subprocess
 
 # YOLOv5 detect.py 실행
-subprocess.run(['python', 'detect.py', '--weights', 'runs/train/exp9/weights/best.pt', '--img', '640', '--conf', '0.5', '--source', '../dataset/real_test/test1.jpg', '--save-txt', '--save-conf'])
+subprocess.run(['python', 'detect.py', '--weights', 'runs/train/exp/weights/best.pt', '--img', '640', '--conf', '0.5', '--source', '../dataset/real_test/gesture.mp4', '--save-txt', '--save-conf'])
